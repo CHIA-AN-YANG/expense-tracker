6addbf76c4f8b71ec46360bec0ec452cceb4bb66
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-05-16 02:49:34 +0800

    :bug: (main.hbs) fix missing category in category filter

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const homeRouter = require('./modules/home');
 const recordRouter = require('./modules/records');
 
-//提供網頁訊息的 middleware
-router.use(function(req, res, next) {
-  const d1 = new Date().toLocaleString()
-  console.log(`method: ${req.method}, router: ${req.url}, time: ${d1}`)
-  next()
-})
+
 
 router.use('/records', recordRouter);
 router.use('/', homeRouter);
