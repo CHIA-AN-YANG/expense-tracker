eb4886d5f812ea199e19fa0a6fb271d1c17f7088
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-05-16 12:48:46 +0800

    :sparkles: (app.js) add connection status middleware

--- app.js
+++ app.js
@@ -22,16 +22,26 @@ require('./config/mongoose')
 
 app.locals.categoryMain = categoryArr;
 
-
+let reqStart, reqEnd
 //提供網頁訊息的 middleware
 app.use(function(req, res, next) {
-  const d1 = new Date().toLocaleString({year: 'numeric', month: 'short', day: 'numeric'})
-  console.log(`method: ${req.method} | router: ${req.url} | time: ${d1}`)
-  next()
+  reqStart = Date.now()
+  res.on('close', () => {
+      reqEnd = Date.now()
+      const d1 = new Date().toLocaleString('zh-TW', {
+        year:'numeric', month:'short',  day:'numeric', 
+        hour12:'false', hour:'2-digit', minute:'2-digit', second:'2-digit'})
+      console.log(`${d1} | ${req.method} from ${req.url} | total time: ${reqEnd-reqStart} ms`)
+  })
+  next()  
 })
 
+
+
 const routes = require('./routes')
-app.use(routes)
+app.use(routes,(req, res, next) => {
+  console.log('end!')} )
+
 
 
 app.listen(PORT, () => {
