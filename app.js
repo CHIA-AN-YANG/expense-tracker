73a592fc758df9c8bacb078d4bb6ba502f0f5623
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-05-16 18:17:33 +0800

    :recycle:  refactor to make code more readable

--- app.js
+++ app.js
@@ -18,31 +18,29 @@ app.set('view engine', 'hbs')
 
 // mongoose
 require('./config/mongoose')
-//對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
 
 app.locals.categoryMain = categoryArr;
 
-let reqStart, reqEnd
 //提供網頁訊息的 middleware
 app.use(function(req, res, next) {
+  let reqStart, reqEnd
   reqStart = Date.now()
   res.on('close', () => {
       reqEnd = Date.now()
       const d1 = new Date().toLocaleString('zh-TW', {
         year:'numeric', month:'short',  day:'numeric', 
-        hour12:'false', hour:'2-digit', minute:'2-digit', second:'2-digit'})
+        hour12:'false', hour:'2-digit', minute:'2-digit', 
+        second:'2-digit'
+      })
       console.log(`${d1} | ${req.method} from ${req.url} | total time: ${reqEnd-reqStart} ms`)
   })
   next()  
 })
 
-
-
+//route setting
 const routes = require('./routes')
 app.use(routes,(req, res, next) => {
   console.log('end!')} )
-
-
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`)
